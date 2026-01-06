7033d00444621407a522138222ed09ec5582dc71
axisdadev <77366347+axisdadev@users.noreply.github.com>
2026-01-06 10:37:10 -0800

    Inital commit, terminal animations, more cool shit

---
+++ app/components/terminal.tsx
@@ -0,0 +1,24 @@
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
+interface TerminalLineProps {
+  text: string;
+  delay?: number;
+  speed?: number;
+  onComplete?: () => void;
+}
+
+function TerminalLine({ text = "", delay = 0, speed = 50, onComplete }: TerminalLineProps) {
+  return (
+    <TypeAnimation
+      sequence={[
+        delay,
+        text,
+        () => onComplete && onComplete()
+      ]}
+      wrapper="div"
+      speed={speed as any}
+      cursor={false}
+    />
+  );
+}
