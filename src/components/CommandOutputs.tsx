a1027b432d75f73ef12793dc63eb87b36795aac2
axisdadev <77366347+axisdadev@users.noreply.github.com>
2026-01-09 13:42:25 -0800

    start working on linux

--- src/components/CommandOutputs.tsx
+++ src/components/CommandOutputs.tsx
@@ -32,27 +32,28 @@ function parseOutput(text: string, className = "text-white", animated = false): 
 
 export const commandOutputs: Record<string, CommandOutput[]> = {
   "about": parseOutput(`
-Hello there! My name is linuxlarp or linuxlarper.
 
-%IMAGE:/Gradient.png%
+%IMAGE: /static/images/pfp.png%
+
+Hello there! My name is linuxlarp or linuxlarper.
 
 Currently 15 years old, self-taught developer, aviation enthusiast, open source & right to repair advocate.
 
-Working on my Private Pilots License (PPL) under the FAA, and enrolled in dual-credit aviation courses at a local higschool.
+Working on my Private Pilots License (PPL) under the FAA, and enrolled in dual-credit aviation courses at a local highschool.
     
 Most of my projects are for Roblox, Discord and social media platforms but im always expanding my skills and expertises to communities, platforms, causes and needs.
 
 I love tinkering and "larping" with Linux as my name suggests. I also love homelabbing on my Raspberry Pi 5 and Dell Optiplex 7010.
 
 And when im not doing any of the above, you can probably find me:
-- Studying
-- Lifting
-- Microsoft FLight Sim
-- Drinking more Caffine becauese: more espresso, less depresso
+- Studying 
+- Lifting 󱅝
+- Microsoft Flight Simulator 
+- Drinking more Caffine becauese: more espresso, less depresso 
 
 To learn more about me, my interests and other topics, you can use any of the following commands:
-about techstack - Tech Experience
-about linux - Linux Experience
+󰦉 about techstack - My Tech Stack / Skills
+ about linux - My Linux Experience and Skills
 
   `, "text-white"),
 
@@ -87,6 +88,17 @@ Applications:
 
 `),
 
+"about linux": parseOutput(`
+I currently daily drive linux for all purposes. I use it for coding, schoolwork, gaming and general usage.
+
+My Linux Setup:
+- Distro: CachyOS x86_64 
+- Kernel: Linux-cachyos 
+- Terminal: ghostty 󱙝
+- WM: Hyprland 
+- Shell: fish 󰈺
+
+`),
 
   projects: parseOutput(`
 
